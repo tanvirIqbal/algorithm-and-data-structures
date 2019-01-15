d38d3413c069cd5161895a2ecea46a998809ec7a
tanviriqbal <tanviriqbal4u@live.com>
2019-01-15 22:21:36 +0600

    Reverse Integer Solution 2

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -14,10 +14,8 @@ function reverseInt(n) {
     .split('')
     .reverse()
     .join('');
-    if (n < 0) {
-        return parseInt(reversed) * -1;
-    }
-    return parseInt(reversed);
+
+    return parseInt(reversed) * Math.sign(n);
 }
 
 module.exports = reverseInt;
