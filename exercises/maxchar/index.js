cd8e7fd57941d827af9f1b8c12122ff0b8d5392f
tanviriqbal <tanviriqbal4u@live.com>
2019-01-15 22:33:48 +0600

    MaxChar similar problems?

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,13 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
+/*
+Similar Problem:
+1. What is the most common character in the string.
+2. Does string A have the same character as string B (is it an anagram)?
+3. Does the given string have any repeated character?
+*/
+
 function maxChar(str) {}
 
 module.exports = maxChar;
